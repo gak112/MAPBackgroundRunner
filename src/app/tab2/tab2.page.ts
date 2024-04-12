2e1581f723c0dc8d698f3d89a6664dd448fd16e9
gak112 <ajay.a02@gmail.com>
2024-04-12 14:14:14 +0530

    lc

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-tab2',
@@ -9,8 +10,21 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  afs: AngularFirestore = inject(AngularFirestore);
+
+  lat: number = 0;
+  lng: number =0;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.afs.doc('locations/1').valueChanges().subscribe((location: any) => {
+      this.lat = location.lat;
+      this.lng = location.lng;
+    });
+  }
 
 }
